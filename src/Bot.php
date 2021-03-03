2b19dbafbec98ea929ae19e884b4d2875f4db8cd
chipslays <hello@chipslays.ru>
2021-03-03 13:55:53 +0400

    [feat] added default config

--- src/Bot.php
+++ src/Bot.php
@@ -42,7 +42,94 @@ class Bot extends Container
     /**
      * @var \Chipslays\Collection\Collection
      */
-    private $config = [];
+    private $config = [
+        'bot' => [
+            'token' => '1234567890:BOT_TOKEN',
+            'handler' => 'https://example.com/handler.php',
+            'name' => 'MyBot',
+            'username' => 'MyTelegram_bot',
+            'version' => '1.0.0',
+            'timezone' => 'UTC',
+        ],
+        'telegram' => [
+            'parse_mode' => 'html',
+            'safe_callback' => true,
+        ],
+        'debug' => [
+            'enable' => false,
+            'developer' => '436432850',
+        ],
+        'admin' => [
+            'list' => [
+                'chipslays' => 'password',
+                '436432850' => 'password',
+            ],
+        ],
+        'modules' => [
+            'database' => [
+                'enable' => false,
+                'driver' => 'mysql',
+                'sqlite' => [
+                    'database' => '/path/to/database.sqlite',
+                ],
+                'mysql' => [
+                    'host'      => 'localhost',
+                    'database'  => 'telegram_test',
+                    'username'  => 'mysql',
+                    'password'  => 'mysql',
+                    'charset'   => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ],
+            ],
+            'cache' => [
+                'enable' => false,
+                'driver' => 'memcached',
+                'memcached' => [
+                    'host'  => 'localhost',
+                    'port' => '11211',
+                ],
+                'redis' => [
+                    'host'  => '127.0.0.1',
+                    'port' => '6379',
+                ],
+            ],
+            'store' => [
+                'enable' => false,
+                'driver' => 'database',
+                'file' => [
+                    'dir' => __DIR__ . '/storage/store',
+                ],
+            ],
+            'user' => [
+                'enable' => false,
+                'flood_time' => 1,
+            ],
+            'state' => [
+                'enable' => false,
+            ],
+            'localization' => [
+                'enable' => false,
+                'default' => 'en',
+                'dir' => __DIR__ . '/localization',
+            ],
+            'logger' => [
+                'enable' => false,
+                'dir' => __DIR__ . '/storage/logs',
+                'auto' => true,
+            ],
+            'statistics' => [
+                'updates' => false,
+                'messages' => true,
+                'users' => true,
+            ],
+        ],
+        'components' => [
+            'vendor.component' => [
+                'enable' => false,
+                'entrypoint' => __DIR__ . '/components/vendor/component/autoload.php',
+            ],
+        ],
+    ];
 
     /**
      * @var array
