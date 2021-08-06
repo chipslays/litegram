e6ead6f3387e770bfa3e01e97df821ce6e3da1f3
chipslays <hello@chipslays.ru>
2021-08-06 16:49:55 +0400

    init v3.x.x

--- examples/localization/en.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'start' => 'Bot launched!',
-];
--- tests/Litegram/EventsTest.php
+++ tests/Litegram/EventsTest.php
@@ -7,17 +7,6 @@ final class EventsTest extends TestCase
 {
     public function testOnByDot()
     {
-        bot()->webhook(Update::START);
-
-        bot()->on('message.text', function () {
-            echo 'catch';
-        });
-
-        ob_start();
-        bot()->run();
-        $output = ob_get_contents();
-        ob_end_clean();
-
-        $this->assertEquals('catch', $output);
+        //
     }
 }
