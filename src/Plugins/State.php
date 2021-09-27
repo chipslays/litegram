671c5ac19bd88d5a719861b65dabf44349c45bfb
chipslays <hello@chipslays.ru>
2021-09-28 02:29:17 +0400

    [feat] refactor `state` plugin

--- src/Plugins/State.php
+++ src/Plugins/State.php
@@ -13,13 +13,8 @@ class State extends AbstractPlugin
      * @var array
      */
     public static $depends = [
-        'storage',
+        'session',
     ];
-
-    /**
-     * @var string
-     */
-    private static $userId;
 
     public static $name = null;
 
@@ -30,77 +25,69 @@ class State extends AbstractPlugin
      */
     public static function boot(): void
     {
-        if (self::$bot->isPluginExists('user')) {
-            self::$userId = User::get('id');
-        } else {
-            self::$userId = self::$payload->get('*.from.id');
-        }
-
         $state = self::get();
         self::$name = $state['name'] ?? null;
         self::$data = $state['data'] ?? null;
     }
 
-    public static function get()
+    /**
+     * @return array
+     */
+    public static function get(): array
     {
-        return self::$userId ? self::getById(self::$userId) : false;
+        return Session::get('litegram:state', ['name' => null, 'data' => null]);
     }
 
     /**
-     * @param int|string $userId
+     * @param string $name
+     * @param mixed $data
+     * @return void
      */
-    public static function getById($userId)
-    {
-        return Storage::get(self::getStateId($userId));
-    }
-
     public static function set($name = null, $data = null): void
     {
-        self::setById(self::$userId, $name, $data);
-    }
-
-    public static function save(): void
-    {
-        self::setById(self::$userId, self::$name, self::$data);
-    }
-
-    public static function setById($userId, $name = null, $data = null)
-    {
-        // we not setting $name & $data directly, but it may error collision with current request...
-        return Storage::set(self::getStateId($userId), [
+        Session::set('litegram:state', [
             'name' => $name,
-            'data' => $data
+            'data' => $data,
         ]);
     }
 
+    /**
+     * @return void
+     */
+    public static function save(): void
+    {
+        self::set(self::$name, self::$data);
+    }
+
+    /**
+     * @return void
+     */
     public static function clear(): void
     {
-        self::clearById(self::$userId);
+        Session::delete('litegram:state');
     }
 
-    public static function clearById($userId)
+    /**
+     * @param string $name
+     * @return void
+     */
+    public static function setName(string $name): void
     {
-        return Storage::delete(self::getStateId($userId));
-    }
-
-    public static function setName($name)
-    {
-        return Storage::set(self::getStateId(self::$userId), [
+        Session::set('litegram:state', [
             'name' => $name,
             'data' => self::$data,
         ]);
     }
 
-    public static function setData($data)
+    /**
+     * @param mixed $data
+     * @return void
+     */
+    public static function setData($data): void
     {
-        return Storage::set(self::getStateId(self::$userId), [
+        Session::set('litegram:state', [
             'name' => self::$name,
             'data' => $data,
         ]);
-    }
-
-    private static function getStateId($userId): string
-    {
-        return "{$userId}__USER__STATE__ID";
     }
 }
